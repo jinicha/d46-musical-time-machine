ba1aef6966fce10b9cdd8e1289999c709a2e8ca9
jinicha <jinichaa@gmail.com>
2021-11-26 21:23:20 -0800

    Put titles in a list

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ BASE_URL = "https://www.billboard.com/charts/hot-100/"
 date = input('Which year do you want to travel to? (in format YYYY-MM-DD): ')
 response = requests.get(f'{BASE_URL}{date}/')
 soup = BeautifulSoup(response.text, "html.parser")
-titles = soup.select("ul li h3")[:100]
-print(titles)
+songs = soup.select("ul li h3")[:100]
+song_title = [song.get_text().strip('\n') for song in songs]
+print(song_title)
